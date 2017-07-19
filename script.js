4c7f0eb30a03f72a847f48dc65caa9f855e83eea
NeonWizard <averagewizard13@gmail.com>
2017-07-19 14:10:05 -0600

    redo box placing to allow boxes growing on mousehover

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function createGrid() {
  	let week = [];
 	for (let day = 0; day < 145; day++) {
 		// clone my mood box template from the DOM
-		let boxElem = document.getElementById("mood-box-template").content.cloneNode(true).querySelector(".mood-box");
+		let boxContainer = document.getElementById("mood-box-template").content.cloneNode(true).children[0];
+		let boxElem = boxContainer.querySelector(".mood-box");
+
 		if (Math.random() < 0.3) {
 			boxElem.style.backgroundColor = "orange";
 		}
 
-		week.push(boxElem);
+		week.push(boxContainer);
 
 		// if a column is done, we then push it to the DOM's grid
 		if (week.length == 6) {
