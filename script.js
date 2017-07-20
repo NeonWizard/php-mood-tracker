1975c50e080f2bec28b15fcf14ab20c20ea342ff
NeonWizard <averagewizard13@gmail.com>
2017-07-19 18:38:46 -0600

    yet ANOTHER approach for display boxes, this time using DOY to numerate each

--- script.js
+++ script.js
@@ -1,62 +1,52 @@
-Date.prototype.monthNames = [
-	"January", "February", "March",
-	"April", "May", "June",
-	"July", "August", "September",
-	"October", "November", "December"
-];
-
-Date.prototype.getMonthName = function() {
-	return this.monthNames[this.getMonth()];
-};
-Date.prototype.getShortMonthName = function () {
-	return this.getMonthName().substr(0, 3);
-};
-Date.prototype.monthDays = function() {
-	let d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
-	return d.getDate();
-}
-
-
 function createGrid() {
 	// clear grid of previous elements
 	while (document.getElementById("mood-grid").firstChild) {
 		document.getElementById("mood-grid").removeChild(document.getElementById("mood-grid").firstChild);
 	}
 
-	let spring = new Date().getMonth() < 6;
+	let today = new Date();
+	let doy = today.getDOY();
+	let startDate = new Date();
+	startDate.setDate(today.getDate() - 182);
+	startDate.setDate(startDate.getDate() - (6-startDate.getDay())); // set startDate to a monday
+	
+	let daysToDisplay = Date.daysBetween(startDate, today)
 
+	// get daily information from server from past daysToDisplay
+	// -- pass --
+
+	// populate grid with boxes
 	let week = [];
-	for (let monthNum = (spring ? 0 : 6); monthNum < (spring ? 6 : 12); monthNum++) {
-		let month = new Date(2017, monthNum);
-		for (let day = 1; day <= month.monthDays(); day++) {
-			// clone my mood box template from the DOM
-			let boxContainer = document.getElementById("mood-box-template").content.cloneNode(true).children[0];
-			let boxElem = boxContainer.querySelector(".mood-box");
+	for (let day = daysToDisplay; day >= 0; day--) {
+		// clone my mood box template from the DOM
+		let boxContainer = document.getElementById("mood-box-template").content.cloneNode(true).children[0];
+		let boxElem = boxContainer.querySelector(".mood-box");
+		// boxElem.innerText = doy - day;
+		boxElem.doy = doy - day;
 
-			week.push(boxContainer);
+		week.push(boxContainer);
 
-			// if a column is done, we then push it to the DOM's grid
-			if (week.length == 7) {
-				// create the new week column element
-				let weekElem = document.createElement("DIV");
-				weekElem.className = "mood-grid-col";
+		// if a column is done, we then push it to the DOM's grid
+		if (week.length == 7) {
+			// create the new week column element
+			let weekElem = document.createElement("DIV");
+			weekElem.className = "mood-grid-col";
 
-				if (day > 3 && day <= 10) {
-					let monthHeader = document.createElement("SPAN");
-					monthHeader.innerHTML = month.getShortMonthName();
-					monthHeader.className = "month-header";
-					weekElem.appendChild(monthHeader);
-				}
-
-				// populate the week element and empty the array
-				for (let boxElem of week) {
-					weekElem.appendChild(boxElem);
-				}
-				week = [];
-
-				// push it to DOM
-				document.getElementById("mood-grid").appendChild(weekElem);
+			if (day > 3 && day <= 10) {
+				let monthHeader = document.createElement("SPAN");
+				// monthHeader.innerHTML = month.getShortMonthName();
+				monthHeader.className = "month-header";
+				weekElem.appendChild(monthHeader);
 			}
+
+			// populate the week element and empty the array
+			for (let boxElem of week) {
+				weekElem.appendChild(boxElem);
+			}
+			week = [];
+
+			// push it to DOM
+			document.getElementById("mood-grid").appendChild(weekElem);
 		}
 	}
 	if (week) {
